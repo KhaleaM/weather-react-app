9109d0c2cc08ac50bc476f2cf90147e284c729d4
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-02 11:53:47 -0500

    added 'bootstrap'

--- src/App.js
+++ src/App.js
@@ -4,26 +4,28 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
-      <footer>
-        This project was coded by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/182572-khalea-mitchell"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Khalea Mitchell
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/KhaleaM/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-        .
-      </footer>
+      <div className="container">
+        <h1>Weather App</h1>
+        <footer>
+          This project was coded by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/182572-khalea-mitchell"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Khalea Mitchell
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/KhaleaM/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+          .
+        </footer>
+      </div>
     </div>
   );
 }
