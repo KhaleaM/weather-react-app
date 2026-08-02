8b2da736d9bf1fe5559a9fe386e4dcc4fbad9a85
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-02 11:39:53 -0500

    added the 'footer'

--- src/App.js
+++ src/App.js
@@ -1,10 +1,29 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 export default function App() {
   return (
     <div className="App">
-      Weather App
+      <h1>Weather App</h1>
+      <footer>
+        This project was coded by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/182572-khalea-mitchell"
+          target="_blank"
+          rel="noreferrer"
+        >
+          Khalea Mitchell
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/KhaleaM/weather-react-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          GitHub
+        </a>
+        .
+      </footer>
     </div>
   );
 }
