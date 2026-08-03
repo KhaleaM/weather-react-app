20e22e909e7bcb1d592691888534244722b71421
KhaleaM <106846809+KhaleaM@users.noreply.github.com>
2026-08-03 17:21:39 -0500

    modified the finishing touches

--- src/Weather.js
+++ src/Weather.js
@@ -1,44 +1,43 @@
 import React, { useState } from "react";
 import WeatherInfo from "./WeatherInfo";
 import WeatherForecast from "./WeatherForecast";
-import axios from "axios"
+import axios from "axios";
 import "./Weather.css";
 export default function Weather(props) {
+  const [weatherData, setWeatherData] = useState({ ready: false });
+  const [city, setCity] = useState(props.defaultCity);
 
-    const [weatherData, setWeatherData] = useState({ ready: false });
-    const [city, setCity] = useState(props.defaultCity);
+  function handleResponse(response) {
+    console.log(response.data);
+    setWeatherData({
+      ready: true,
+      coordinates: response.data.coordinates,
+      temperature: response.data.temperature,
+      humidity: response.data.temperature.humidity,
+      date: new Date(response.data.time * 1000),
+      wind: response.data.wind,
+      city: response.data.city,
+      description: response.data.condition.description,
+      icon: response.data.condition.icon,
+    });
+  }
 
-    function handleResponse(response) {
-        console.log(response.data);
-        setWeatherData({
-            ready: true,
-            coordinates: response.data.coordinates,
-          temperature: response.data.temperature,
-          humidity: response.data.temperature.humidity,
-          date: new Date(response.data.time * 1000),
-          wind: response.data.wind,
-          city: response.data.city,
-          description: response.data.condition.description,
-          icon: response.data.condition.icon,
-        });
-    }
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-    function handleSubmit(event) {
-        event.preventDefault();
-       search();
-    }
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
-    function handleCityChange(event) {
-        setCity(event.target.value);
-    }
+  function search() {
+    const apiKey = "e2t5dd99b4cfa003ed9aaof63c44d73c";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+  }
 
-    function search() {
-      const apiKey = "e2t5dd99b4cfa003ed9aaof63c44d73c";
-      let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
-      axios.get(apiUrl).then(handleResponse);
-    }
-
-   if (weatherData.ready) {
+  if (weatherData.ready) {
     return (
       <div className="weather">
         <form onSubmit={handleSubmit}>
@@ -65,9 +64,8 @@ export default function Weather(props) {
         <WeatherForecast coordinates={weatherData.coordinates} />
       </div>
     );
-   } else {
+  } else {
     search();
     return "Loading Weather...";
-   }
-
+  }
 }
--- src/WeatherForecastDay.js
+++ src/WeatherForecastDay.js
@@ -1,38 +1,33 @@
 import React from "react";
-import WeatherIcon from "./WeatherIcon"
+import WeatherIcon from "./WeatherIcon";
 export default function WeatherForecastDay(props) {
-    function maxTemperature() {
-        let temperature = Math.round(props.data.temperature.maximum);
-        return `${temperature}°`;
-    }
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
 
-     function minTemperature() {
-        let temperature = Math.round(props.data.temperature.minimum);
-        return `${temperature}°`;
-    }
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
 
-    function day() {
-        let date = new Date(props.data.time * 1000);
-        let day = date.getDay();
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
 
-        let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+    let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
 
-        return days[day];
-    }
+    return days[day];
+  }
 
-
-    return (
-      <div>
-        <div className="weatherForecastDay">{day()}</div>
-        <WeatherIcon code={props.data.condition.icon} size={36} />
-        <div className="weatherForecastTemp">
-          <span className="weatherForecastMax">
-            {maxTemperature()}
-          </span>
-          <span className="weatherForecastMin">
-            {minTemperature()}
-          </span>
-        </div>
+  return (
+    <div>
+      <div className="weatherForecastDay">{day()}</div>
+      <WeatherIcon code={props.data.condition.icon} size={36} />
+      <div className="weatherForecastTemp">
+        <span className="weatherForecastMax">{maxTemperature()}</span>
+        <span className="weatherForecastMin">{minTemperature()}</span>
       </div>
-    );
+    </div>
+  );
 }
